2cf56a48bcf4cc75e1aa3bb160278d6f0b192e7b
Daniel Molina <dmolcap@gmail.com>
2021-10-10 22:28:01 +0200

    Add tests one player enter and minimum amount

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -20,6 +20,20 @@ describe('Lottery Contract', () => {
       assert.ok(lottery.options.address);
    });
 
+   it('allows one account to enter', async () => {
+      await lottery.methods.enter().send({
+         from: accounts[0],
+         value: web3.utils.toWei('0.02', 'ether')
+      });
+
+      const players = await lottery.methods.getPlayers().call({
+         from: accounts[0]
+      });
+
+      assert.equal(accounts[0], players[0]);
+      assert.equal(1, players.length);
+   });
+
    it('allows multiple accounts to enter', async () => {
       await lottery.methods.enter().send({
          from: accounts[0],
@@ -44,5 +58,17 @@ describe('Lottery Contract', () => {
       assert.equal(accounts[1], players[1]);
       assert.equal(accounts[2], players[2]);
       assert.equal(3, players.length);
+   });
+
+   it('require a minimum amount of ether to enter', async () => {
+      try {
+         await lottery.methods.enter().send({
+            from: accounts[0],
+            value: 100
+         });
+         assert(false);
+      } catch (err) {
+         assert(err);
+      };
    });
 });
